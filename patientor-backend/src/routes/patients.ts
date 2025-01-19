d3dcae6eaee032452a8da51aafd673adc716ec41
tumffa <tuomas.v.niemi@gmail.com>
2025-01-19 11:26:33 +0200

    ex 9.14

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,5 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { ZodError } from 'zod';
 import patientsService from '../services/patientService';
+import { newPatientSchema } from '../utils';
+import { NewPatient, Patient } from '../types';
 
 const patientsRouter = express.Router();
 
@@ -7,8 +10,24 @@ patientsRouter.get('/', (_req, res) => {
   res.send(patientsService.getCensoredPatients());
 });
 
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-patientsRouter.post('/', (req, res) => {
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    newPatientSchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+patientsRouter.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
   const { name, dateOfBirth, ssn, gender, occupation } = req.body;
   const newPatient = patientsService.addPatient({
     name,
@@ -19,5 +38,7 @@ patientsRouter.post('/', (req, res) => {
   });
   res.json(newPatient);
 });
+
+patientsRouter.use(errorMiddleware);
 
 export default patientsRouter;
